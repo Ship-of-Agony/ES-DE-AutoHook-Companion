b9ea1a5448bf72de9879756b6e7ef76a2f0cd971
Ship-of-Agony <287680459+Ship-of-Agony@users.noreply.github.com>
2026-08-26 17:39:37 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "org.esde.autohook.companion"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.esde.autohook.companion"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+}
